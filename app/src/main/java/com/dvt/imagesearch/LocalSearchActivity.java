0a644b3727ba6680a4137bcf982fe554278b60d1
Doan Trung <doanpt93cntt@gmail.com>
2015-11-05 08:57:00 +0700

    search local image

--- app/src/main/java/com/dvt/imagesearch/LocalSearchActivity.java
+++ app/src/main/java/com/dvt/imagesearch/LocalSearchActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -12,7 +11,6 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import com.dvt.adapters.ItemImageLocal;
-import com.dvt.adapters.ListViewImageAdapter;
 import com.dvt.adapters.ListViewLocalAdapter;
 
 import java.io.File;
@@ -21,7 +19,7 @@ import java.util.ArrayList;
 /**
  * Created by doantrung on 11/2/15.
  */
-public class LocalSearchActivity extends Activity implements View.OnClickListener{
+public class LocalSearchActivity extends Activity implements View.OnClickListener {
     public static final String KEY_ARRAY_IMAGE_LOCAL = "key_array_image_l";
     public static final String KEY_POSITION_LOCAL = "key_position_local";
     private EditText edtContentSearchLocal;
@@ -47,7 +45,7 @@ public class LocalSearchActivity extends Activity implements View.OnClickListene
         lvImageLocal = (ListView) findViewById(R.id.lv_image_local);
         arrImage = getAllImageLocal();
         adapter = new ListViewLocalAdapter(activity, arrImage);
-        arrImageResultSearch=new ArrayList<>();
+        arrImageResultSearch = new ArrayList<>();
         lvImageLocal.setAdapter(adapter);
         lvImageLocal.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -69,7 +67,7 @@ public class LocalSearchActivity extends Activity implements View.OnClickListene
         String path = Environment.getExternalStoragePublicDirectory(DownloadDialog.MY_FOLDER).toString();
         File file = new File(path);
         File listFile[] = file.listFiles();
-        int length=listFile.length;
+        int length = listFile.length;
         for (int i = 0; i < length; i++) {
             listImage.add(i, new ItemImageLocal(listFile[i].getName(), listFile[i].getPath()));
         }
@@ -78,7 +76,7 @@ public class LocalSearchActivity extends Activity implements View.OnClickListene
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_search_local:
                 searchLocal();
                 break;
@@ -87,33 +85,32 @@ public class LocalSearchActivity extends Activity implements View.OnClickListene
 
     private void searchLocal() {
         arrImageResultSearch.clear();
-        if(edtContentSearchLocal.getText().equals("")){
-            arrImage=getAllImageLocal();
-            arrImageResultSearch=arrImage;
+        arrImage = getAllImageLocal();
+        if (edtContentSearchLocal.getText().equals("")) {
+            arrImageResultSearch = arrImage;
             displayListImage(arrImageResultSearch);
-        }else{
-            String key=edtContentSearchLocal.getText().toString();
-            searchLocalImage(key);
-        }
-    }
-
-    private void searchLocalImage(String key) {
-        int size=arrImage.size();
-        for(int i=0;i<size;i++){
-            if(arrImage.get(i).getImageName().contains(key)){
-                arrImageResultSearch.add(arrImage.get(i));
+        } else {
+            String key = edtContentSearchLocal.getText().toString();
+            key = key.replaceAll("[^a-zA-Z]+", "").trim();
+            int size = arrImage.size();
+            String imageName = "";
+            for (int i = 0; i < size; i++) {
+                imageName = arrImage.get(i).getImageName().replaceAll("[^a-zA-Z]+", "").trim();
+                if (imageName.indexOf(key) != -1) {
+                    arrImageResultSearch.add(arrImage.get(i));
+                }
             }
-        }
-        if(arrImageResultSearch.size()>0){
-            arrImage=arrImageResultSearch;
-            displayListImage(arrImageResultSearch);
-        }else{
-            displayListImage(arrImage);
+            if (arrImageResultSearch.size() > 0) {
+                arrImage = arrImageResultSearch;
+                displayListImage(arrImageResultSearch);
+            } else {
+                displayListImage(arrImage);
+            }
         }
     }
 
     private void displayListImage(ArrayList<ItemImageLocal> listImage) {
-        lvImageLocal.setAdapter(new ListViewLocalAdapter(this,listImage));
+        lvImageLocal.setAdapter(new ListViewLocalAdapter(this, listImage));
         adapter.notifyDataSetChanged();
     }
 }
