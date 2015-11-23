0725885e36340def72f10c9fe404b843d995f65f
Doan Trung <doanpt93cntt@gmail.com>
2015-11-23 14:55:02 +0700

    init project video search using youtube data api

---
+++ app/src/main/java/com/framgia/ytbsearch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.framgia.ytbsearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
