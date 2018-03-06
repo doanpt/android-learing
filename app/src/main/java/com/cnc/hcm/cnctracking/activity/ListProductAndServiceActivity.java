bb012185bab6db512cd7338fe3f9a6be9cd152ed
doan.pt <doanpt93cntt@gmail.com>
2018-03-06 23:38:51 +0700

    load category

--- app/src/main/java/com/cnc/hcm/cnctracking/activity/ListProductAndServiceActivity.java
+++ app/src/main/java/com/cnc/hcm/cnctracking/activity/ListProductAndServiceActivity.java
@@ -8,16 +8,30 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.cnc.hcm.cnctracking.R;
 import com.cnc.hcm.cnctracking.adapter.FragmentAdapter;
+import com.cnc.hcm.cnctracking.api.ApiUtils;
+import com.cnc.hcm.cnctracking.api.MHead;
 import com.cnc.hcm.cnctracking.fragment.ListProductFragment;
 import com.cnc.hcm.cnctracking.fragment.ListServiceFragment;
+import com.cnc.hcm.cnctracking.model.CategoryListResult;
+import com.cnc.hcm.cnctracking.model.ProductListResult;
 import com.cnc.hcm.cnctracking.util.Conts;
+import com.cnc.hcm.cnctracking.util.UserInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ListProductAndServiceActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -27,11 +41,30 @@ public class ListProductAndServiceActivity extends AppCompatActivity implements 
     private ViewPager viewPager;
     private TextView tvClear;
     private OnTextChangeProductListener onTextChangeProductListener;
+    private ArrayAdapter manufactureAdapter;
+    private ArrayAdapter categoryAdapter;
+    private ArrayList<String> arrManufactures;
+    private ArrayList<String> arrCategory;
+    private List<MHead> arrHeads;
+    private String accessToken;
+    private ArrayList<ProductListResult.Product> listProduct;
+    private ArrayList<CategoryListResult.Category> listCategory;
+    private Spinner spnManufacuture, spnCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_product_and_service);
+        arrHeads = new ArrayList<>();
+        arrManufactures = new ArrayList<>();
+        arrCategory = new ArrayList<>();
+        manufactureAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, arrManufactures);
+        manufactureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoryAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, arrCategory);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accessToken = UserInfo.getInstance(getApplicationContext()).getAccessToken();
+        arrHeads.clear();
+        arrHeads.add(new MHead(Conts.KEY_ACCESS_TOKEN, accessToken));
         initViews();
     }
 
@@ -42,6 +75,48 @@ public class ListProductAndServiceActivity extends AppCompatActivity implements 
         llSearchVisiable = findViewById(R.id.ll_search_visible);
         tvClear = (TextView) findViewById(R.id.tv_clear_text_search);
         edtSearch = (EditText) findViewById(R.id.edt_search);
+        ApiUtils.getAPIService(arrHeads).getListManufactures().enqueue(new Callback<ProductListResult>() {
+            @Override
+            public void onResponse(Call<ProductListResult> call, Response<ProductListResult> response) {
+                Long status = response.body().getStatusCode();
+                if (status == 200) {
+                    arrManufactures.clear();
+                    listProduct = (ArrayList<ProductListResult.Product>) response.body().getResult();
+                    for (ProductListResult.Product p : listProduct) {
+                        arrManufactures.add(p.getName());
+                        manufactureAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProductListResult> call, Throwable t) {
+
+            }
+        });
+        ApiUtils.getAPIService(arrHeads).getListCategory().enqueue(new Callback<CategoryListResult>() {
+            @Override
+            public void onResponse(Call<CategoryListResult> call, Response<CategoryListResult> response) {
+                Long status = response.body().getStatusCode();
+                if (status == 200) {
+                    arrCategory.clear();
+                    listCategory = (ArrayList<CategoryListResult.Category>) response.body().getResult();
+                    for (CategoryListResult.Category category : listCategory) {
+                        arrCategory.add(category.getTitle());
+                        categoryAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CategoryListResult> call, Throwable t) {
+
+            }
+        });
+        spnCategory = findViewById(R.id.spn_product_type);
+        spnManufacuture = findViewById(R.id.spn_manufacture);
+        spnManufacuture.setAdapter(manufactureAdapter);
+        spnCategory.setAdapter(categoryAdapter);
 
         imgSearch.setOnClickListener(this);
         imvBack.setOnClickListener(this);
