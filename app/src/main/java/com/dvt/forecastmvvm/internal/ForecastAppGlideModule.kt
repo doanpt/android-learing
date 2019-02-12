6728b7eaafa9d6f241938c286e07590427698e7c
Trung Doan <doanpt93cntt@gmail.com>
2019-02-12 14:25:29 +0700

    current weather user interface

---
+++ app/src/main/java/com/dvt/forecastmvvm/internal/ForecastAppGlideModule.kt
@@ -0,0 +1,9 @@
+package com.dvt.forecastmvvm.internal
+
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+class ForecastAppGlideModule : AppGlideModule()
