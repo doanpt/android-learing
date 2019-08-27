b5af6d48d2e833c439bc70f9062e9eb414156571
Trung Doan <doanpt93cntt@gmail.com>
2019-08-27 21:18:48 +0700

    add navigation library and navigate between fragments

--- Travia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Travia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,7 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 /**
@@ -21,6 +24,12 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+//        binding.playButton.setOnClickListener { view: View ->
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 
