6256b9981f4a363b49517a5dfee749747cf38970
Trung Doan <doanpt93cntt@gmail.com>
2019-08-27 21:53:30 +0700

    add menu option in actionbar

--- Travia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Travia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -26,6 +26,7 @@ import com.example.android.navigation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         var navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
