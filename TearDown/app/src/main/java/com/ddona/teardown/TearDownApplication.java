b5a985b60b98460c88998b80f35943c0aa369756
Trung Doan <doanpt93cntt@gmail.com>
2019-08-29 21:46:39 +0700

    add timber library for logging

---
+++ TearDown/app/src/main/java/com/ddona/teardown/TearDownApplication.java
@@ -0,0 +1,13 @@
+package com.ddona.teardown;
+
+import android.app.Application;
+import timber.log.Timber;
+
+public class TearDownApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.
+    }
+}
