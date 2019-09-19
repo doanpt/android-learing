bea4fa316fbbf8ba2453268dcfbf3655a1520936
Trung Doan <doanpt93cntt@gmail.com>
2019-09-19 22:12:17 +0700

    move data from fragment to viewmodel

--- Lesson5Tour/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lesson5Tour/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -4,8 +4,75 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
     init {
         Log.i("GameViewModel", "Game view model created")
+        resetList()
+        nextWord()
+    }
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //FIXME need to fix
+            // gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
     }
 
     override fun onCleared() {
