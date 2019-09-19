80b7e24a40af3de366d321d3f2df893a8932f39d
Trung Doan <doanpt93cntt@gmail.com>
2019-09-19 22:30:39 +0700

    use live data for score field

--- Lesson5Tour/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ Lesson5Tour/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -23,6 +23,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
@@ -52,16 +53,15 @@ class GameFragment : Fragment() {
 
         binding.correctButton.setOnClickListener {
             viewModel.onCorrect()
-            updateScoreText()
             updateWordText()
         }
         binding.skipButton.setOnClickListener {
             viewModel.onSkip()
-            updateScoreText()
             updateWordText()
         }
-        updateScoreText()
-        updateWordText()
+        viewModel.score.observe(this, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
         return binding.root
 
     }
@@ -71,7 +71,7 @@ class GameFragment : Fragment() {
      * Called when the game is finished
      */
     private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(viewModel.score)
+        val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
         findNavController(this).navigate(action)
     }
 
@@ -80,9 +80,5 @@ class GameFragment : Fragment() {
     private fun updateWordText() {
         binding.wordText.text = viewModel.word
 
-    }
-
-    private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.toString()
     }
 }
--- Lesson5Tour/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lesson5Tour/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
@@ -9,12 +10,13 @@ class GameViewModel : ViewModel() {
     var word = ""
 
     // The current score
-    var score = 0
+    var score = MutableLiveData<Int>()
 
     init {
         Log.i("GameViewModel", "Game view model created")
         resetList()
         nextWord()
+        score.value = 0
     }
 
     // The list of words - the front of the list is the next word to guess
@@ -66,12 +68,12 @@ class GameViewModel : ViewModel() {
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        score.value = score.value?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        score.value = score.value?.plus(1)
         nextWord()
     }
 
