4cad835c445b2ec7d252bae6300d6a3c66245f1b
Trung Doan <doanpt93cntt@gmail.com>
2019-09-27 21:50:36 +0700

    Add Sleepnight Entity for room database

--- Lesson6SleepTracker/app/src/main/java/com/ddona/sleep/database/SleepNight.kt
+++ Lesson6SleepTracker/app/src/main/java/com/ddona/sleep/database/SleepNight.kt
@@ -15,3 +15,19 @@
  */
 
 package com.ddona.sleep.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "sleep_night")
+data class SleepNight(
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+    @ColumnInfo(name = "start_time_milli")
+    var startTimeMilli: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMilli: Long = startTimeMilli,
+    @ColumnInfo(name = "quality_ratting")
+    var sleepQuality: Int = -1
+)
