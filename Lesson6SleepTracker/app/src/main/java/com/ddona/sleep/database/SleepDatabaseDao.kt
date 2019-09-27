8416d91a706e6055d7edaf732caae275cc505c42
Trung Doan <doanpt93cntt@gmail.com>
2019-09-27 21:59:51 +0700

    add Sleep night DAO for room

--- Lesson6SleepTracker/app/src/main/java/com/ddona/sleep/database/SleepDatabaseDao.kt
+++ Lesson6SleepTracker/app/src/main/java/com/ddona/sleep/database/SleepDatabaseDao.kt
@@ -16,4 +16,29 @@
 
 package com.ddona.sleep.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SleepDatabaseDao {
+    @Insert
+    fun insert(sleepNight: SleepNight)
+
+    @Update
+    fun update(sleepNight: SleepNight)
+
+    @Delete()
+    fun delete(nights: List<SleepNight>): Int
+
+    @Query("SELECT * from daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllSleepNight(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * from daily_sleep_quality_table where nightId = :key")
+    fun get(key: Long)
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+}
--- Lesson6SleepTracker/app/src/main/java/com/ddona/sleep/database/SleepNight.kt
+++ Lesson6SleepTracker/app/src/main/java/com/ddona/sleep/database/SleepNight.kt
@@ -20,7 +20,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "sleep_night")
+@Entity(tableName = "daily_sleep_quality_table")
 data class SleepNight(
     @PrimaryKey(autoGenerate = true)
     var nightId: Long = 0L,
