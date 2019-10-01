3dde5a02dedf0072b8b84e4893546bfe9104cacc
Trung Doan <doanpt93cntt@gmail.com>
2019-10-01 22:20:35 +0700

    change bindViewHoldeR

--- Lesson7Recycle/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Lesson7Recycle/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -43,8 +44,13 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
-        val sleepNight = data[position]
-        holder.textView.text = sleepNight.sleepQuality.toString()
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+        if (item.sleepQuality <= 1) {
+            holder.textView.setTextColor(Color.RED)
+        } else {
+            holder.textView.setTextColor(Color.BLACK)
+        }
     }
 
 }
