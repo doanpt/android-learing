fc9bf06d88eeb5d284069a4806e8d27eab03a6ea
Trung Doan <doanpt93cntt@gmail.com>
2019-10-05 20:49:30 +0700

    use Grid layout manager

--- Lesson7Recycle/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ Lesson7Recycle/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -67,6 +68,9 @@ class SleepTrackerFragment : Fragment() {
         val adapter = SleepNightAdapter()
 
         binding.sleepNight.adapter = adapter
+
+        val layoutManager = GridLayoutManager(context, 3)
+        binding.sleepNight.layoutManager = layoutManager
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
