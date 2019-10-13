b2bec108b590742584655b139131b92a046cf233
Trung Doan <doanpt93cntt@gmail.com>
2019-10-13 20:55:48 +0700

    display an image to imageview

--- Day8Internet/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Day8Internet/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -26,9 +26,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.lang.Exception
 
 /**
@@ -40,11 +37,16 @@ class OverviewViewModel : ViewModel() {
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status String
-    val response: LiveData<String>
-        get() = _response
+    val status: LiveData<String>
+        get() = _status
+
+    private val _property = MutableLiveData<MarsProperty>()
+
+    val property: LiveData<MarsProperty>
+        get() = _property
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -61,9 +63,11 @@ class OverviewViewModel : ViewModel() {
             var getPropertyDeffer = MarsApi.retrofitService.getProperties()
             try {
                 var listResult = getPropertyDeffer.await()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                if (listResult.isNotEmpty()) {
+                    _property.value = listResult[0]
+                }
             } catch (e: Exception) {
-                _response.value = "Failure: ${e.message}"
+                _status.value = "Failure: ${e.message}"
             }
         }
     }
