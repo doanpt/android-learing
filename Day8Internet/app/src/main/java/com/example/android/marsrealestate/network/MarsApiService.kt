81c63529a87a4b0a227932d52356c60ed8c81c4b
Trung Doan <doanpt93cntt@gmail.com>
2019-10-13 20:23:34 +0700

    add convert factory to convert data

--- Day8Internet/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Day8Internet/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,21 +17,27 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
         .build()
 
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties(): Call<String>
+    fun getProperties(): Call<List<MarsProperty>>
 }
 
 object MarsApi {
