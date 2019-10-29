914557141de017e23f5030f32cd2f17bb241ce63
Trung Doan <doanpt93cntt@gmail.com>
2019-10-29 22:14:39 +0700

    add room database

--- Day9Repository/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ Day9Repository/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,11 +17,9 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface VideoDao {
@@ -30,4 +28,22 @@ interface VideoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideoDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideoDatabase
+
+fun getDatabase(context: Context): VideoDatabase {
+    synchronized(VideoDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideoDatabase::class.java,
+                "videos").build()
+        }
+    }
+    return INSTANCE
 }
