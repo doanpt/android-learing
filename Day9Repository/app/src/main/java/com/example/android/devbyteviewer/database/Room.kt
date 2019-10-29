2e46348502dfde47e8509a9eb12f79789555c058
Trung Doan <doanpt93cntt@gmail.com>
2019-10-29 22:08:54 +0700

    Add video dao

--- Day9Repository/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ Day9Repository/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -16,3 +16,18 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("select * from databasevideo")
+    fun getVideo(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
