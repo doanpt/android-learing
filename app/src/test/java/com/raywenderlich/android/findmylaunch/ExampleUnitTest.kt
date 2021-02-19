b62fc8f22a2c4170f4e07b2cf8b580e239343c7a
Trung Doan <doanpt93cntt@gmail.com>
2021-02-19 16:36:34 +0700

    add unit test cases

---
+++ app/src/test/java/com/raywenderlich/android/findmylaunch/ExampleUnitTest.kt
@@ -0,0 +1,34 @@
+package com.raywenderlich.android.findmylaunch
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun addition_isCorrect0() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun addition_isCorrect2() {
+        assertEquals(4, 5 + 2)
+    }
+    @Test
+    fun addition_isCorrect3() {
+        assertEquals(4, 8 + 2)
+    }
+    @Test
+    fun addition_isCorrect4() {
+        assertEquals(4, 2 + 2)
+    }
+}
