7f0c370fff445d834c668b7a8cd96b3ab861f6a8
Trung Doan <doanpt93cntt@gmail.com>
2021-07-29 14:18:11 +0700

    using logging interceptor to print log of http request

--- app/src/main/java/com/ddona/pokedex/network/retrofit/JsonApiClient.kt
+++ app/src/main/java/com/ddona/pokedex/network/retrofit/JsonApiClient.kt
@@ -1,13 +1,23 @@
 package com.ddona.pokedex.network.retrofit
 
 import com.ddona.pokedex.util.Const
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object JsonApiClient {
     private fun retrofit(): Retrofit {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .baseUrl(Const.BASE_JSON_API_URL)
             .build()
     }
