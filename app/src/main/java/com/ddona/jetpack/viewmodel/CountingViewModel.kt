9d7756ec78be34c99628d4cfb7c6b771896673d3
Trung Doan <doanpt93cntt@gmail.com>
2021-08-08 10:44:46 +0700

    Using method reference in data binding

--- app/src/main/java/com/ddona/jetpack/viewmodel/CountingViewModel.kt
+++ app/src/main/java/com/ddona/jetpack/viewmodel/CountingViewModel.kt
@@ -2,6 +2,7 @@ package com.ddona.jetpack.viewmodel
 
 import android.app.Application
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -18,7 +19,7 @@ class CountingViewModel(application: Application) : ViewModel() {
         count.postValue(count.value?.plus(1))
     }
 
-    fun makeToast() {
+    fun makeToast(view: View) {
         Log.d("doanpt", "your message is: ${message.value}")
     }
 }
