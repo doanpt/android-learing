3bf21d444e48126118d2994888d1aae812bc8303
Trung Doan <doanpt93cntt@gmail.com>
2021-08-11 22:48:48 +0700

    Navigate with action bar and toolbar

--- app/src/main/java/com/ddona/jetpack/ui/nav/NavScreenActivity.kt
+++ app/src/main/java/com/ddona/jetpack/ui/nav/NavScreenActivity.kt
@@ -2,11 +2,30 @@ package com.ddona.jetpack.ui.nav
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.ddona.jetpack.R
+import com.ddona.jetpack.databinding.ActivityNavBinding
 
-class NavScreenActivity: AppCompatActivity() {
+class NavScreenActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityNavBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nav)
+        binding = ActivityNavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/ddona/jetpack/ui/nav/WelcomeFragment.kt
+++ app/src/main/java/com/ddona/jetpack/ui/nav/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.ddona.jetpack.R
 import com.ddona.jetpack.databinding.FragmentWelcomeBinding
@@ -22,6 +23,10 @@ class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
         binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         binding.tvUserName.text = args.username
         binding.tvPassword.text = args.password
+        binding.buttonOk.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }
         return binding.root
     }
 
