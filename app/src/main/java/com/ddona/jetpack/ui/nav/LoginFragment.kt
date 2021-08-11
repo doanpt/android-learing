72e5ca28d229a4e1cc55efea55b95caa392a6309
Trung Doan <doanpt93cntt@gmail.com>
2021-08-11 22:21:46 +0700

    pass data between fragments

--- app/src/main/java/com/ddona/jetpack/ui/nav/LoginFragment.kt
+++ app/src/main/java/com/ddona/jetpack/ui/nav/LoginFragment.kt
@@ -1,14 +1,32 @@
 package com.ddona.jetpack.ui.nav
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ddona.jetpack.R
+import com.ddona.jetpack.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private lateinit var binding: FragmentLoginBinding
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.btnConfirm.setOnClickListener {
+            val username = binding.edtUsername.text.toString()
+            val password = binding.edtPassword.text.toString()
+            val action =
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(username, password)
+            findNavController().navigate(action)
+        }
+        return binding.root
     }
+
 }
--- app/src/main/java/com/ddona/jetpack/ui/nav/WelcomeFragment.kt
+++ app/src/main/java/com/ddona/jetpack/ui/nav/WelcomeFragment.kt
@@ -1,14 +1,28 @@
 package com.ddona.jetpack.ui.nav
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.ddona.jetpack.R
+import com.ddona.jetpack.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
 
+    private lateinit var binding: FragmentWelcomeBinding
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private val args: WelcomeFragmentArgs by navArgs()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        binding.tvUserName.text = args.username
+        binding.tvPassword.text = args.password
+        return binding.root
     }
+
 }
