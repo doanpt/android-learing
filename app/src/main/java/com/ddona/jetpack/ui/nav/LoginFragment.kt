ad6646c1854a376ae51291285e8eca2c6b322ffb
Trung Doan <doanpt93cntt@gmail.com>
2021-08-11 23:20:27 +0700

    Redirect to urls with implicit deep links

--- app/src/main/java/com/ddona/jetpack/ui/nav/LoginFragment.kt
+++ app/src/main/java/com/ddona/jetpack/ui/nav/LoginFragment.kt
@@ -6,19 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.ddona.jetpack.R
 import com.ddona.jetpack.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding: FragmentLoginBinding
+    private val args: LoginFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        val usernameDeepLink = args.username
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.edtUsername.setText(usernameDeepLink)
         binding.btnConfirm.setOnClickListener {
             val username = binding.edtUsername.text.toString()
             val password = binding.edtPassword.text.toString()
