65205cd8a556840d77cbf613360b1cae10633c98
Trung Doan <doanpt93cntt@gmail.com>
2021-08-11 22:00:10 +0700

    Navigate to another fragment

--- app/src/main/java/com/ddona/jetpack/ui/nav/HomeFragment.kt
+++ app/src/main/java/com/ddona/jetpack/ui/nav/HomeFragment.kt
@@ -1,7 +1,26 @@
 package com.ddona.jetpack.ui.nav
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ddona.jetpack.R
+import com.ddona.jetpack.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.buttonLogin.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+        return binding.root
+    }
 }
