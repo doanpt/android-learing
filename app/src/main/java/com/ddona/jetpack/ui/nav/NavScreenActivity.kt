d282adf2d66e0796dc68ae28e1eb199d3bbfc3e5
Trung Doan <doanpt93cntt@gmail.com>
2021-08-11 23:06:46 +0700

    Navigation with bottom navigation view

--- app/src/main/java/com/ddona/jetpack/ui/nav/NavScreenActivity.kt
+++ app/src/main/java/com/ddona/jetpack/ui/nav/NavScreenActivity.kt
@@ -9,25 +9,35 @@ import androidx.navigation.NavGraphNavigator
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ddona.jetpack.R
 import com.ddona.jetpack.databinding.ActivityNavBinding
 
 class NavScreenActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var binding: ActivityNavBinding
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNavBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
+        //create appbar to remove back button on search fragment
+        appBarConfig = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.searchFragment)
+        )
+
         setSupportActionBar(binding.toolbar)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
