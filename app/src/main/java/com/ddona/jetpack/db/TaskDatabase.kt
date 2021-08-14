8bd672c9011303e17af1162d77e8c8a80fe6c8e1
Trung Doan <doanpt93cntt@gmail.com>
2021-08-14 23:39:46 +0700

    Update task database callback to insert fake data in first time database created

--- app/src/main/java/com/ddona/jetpack/db/TaskDatabase.kt
+++ app/src/main/java/com/ddona/jetpack/db/TaskDatabase.kt
@@ -6,6 +6,8 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.ddona.jetpack.model.Task
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 //exportSchema true will let room export data base schema to a folder, it is good for history tracking
 @Database(entities = [Task::class], exportSchema = false, version = 1)
@@ -34,31 +36,35 @@ abstract class TaskDatabase : RoomDatabase() {
         private var instance: TaskDatabase? = null
 
         fun getInstance(
-            context: Context
+            context: Context,
+            scope: CoroutineScope
         ): TaskDatabase = instance ?: synchronized(this) {
-            instance ?: buildDatabase(context).also { instance = it }
+            instance ?: buildDatabase(context, scope).also { instance = it }
         }
 
-        private fun buildDatabase(context: Context): TaskDatabase {
+        private fun buildDatabase(context: Context, scope: CoroutineScope): TaskDatabase {
             return Room.databaseBuilder(context, TaskDatabase::class.java, "task_database")
                 .fallbackToDestructiveMigration()
-                .addCallback(roomCallback)
+                .addCallback(TaskDatabaseCallback(scope))
                 .build()
         }
 
-        private val roomCallback = object : Callback() {
-            override fun onCreate(db: SupportSQLiteDatabase) {
-                super.onCreate(db)
+    }
+
+    private class TaskDatabaseCallback(private val scope: CoroutineScope) :
+        RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            scope.launch {
                 populateDatabase(instance!!)
             }
         }
 
-        private fun populateDatabase(db: TaskDatabase) {
+        private suspend fun populateDatabase(db: TaskDatabase) {
             val taskDao = db.taskDao()
             taskDao.insertTask(Task(1, "Task 1", "Description 1", System.currentTimeMillis()))
             taskDao.insertTask(Task(2, "Task 2", "Description 2", System.currentTimeMillis()))
             taskDao.insertTask(Task(3, "Task 3", "Description 3", System.currentTimeMillis()))
-
         }
     }
 }
