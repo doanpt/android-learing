04b2075fa33191df642a1c91c756bfe1d71c039a
Trung Doan <doanpt93cntt@gmail.com>
2021-08-15 09:49:47 +0700

    Inject by Field Injection

--- app/src/main/java/com/ddona/jetpack/model/Car.kt
+++ app/src/main/java/com/ddona/jetpack/model/Car.kt
@@ -4,7 +4,10 @@ import android.util.Log
 import javax.inject.Inject
 
 class Car @Inject constructor() {
+    @Inject
+    lateinit var driver: Driver
+
     fun drive() {
-        Log.d("doanpt", "Car is running")
+        Log.d("doanpt", "Car is running by driver ${driver.getName()}")
     }
 }
---
+++ app/src/main/java/com/ddona/jetpack/model/Driver.kt
@@ -0,0 +1,10 @@
+package com.ddona.jetpack.model
+
+import javax.inject.Inject
+
+class Driver @Inject constructor() {
+
+    fun getName(): String {
+        return "Trung Doan"
+    }
+}
