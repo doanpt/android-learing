0bd17585518777eab23ae44001c0d0f8bb185e81
Trung Doan <doanpt93cntt@gmail.com>
2021-08-15 15:23:19 +0700

    Insert 10000 records to database in first time database created

--- app/src/main/java/com/ddona/jetpack/di/DatabaseModule.kt
+++ app/src/main/java/com/ddona/jetpack/di/DatabaseModule.kt
@@ -45,9 +45,9 @@ object DatabaseModule {
     }
 
     private suspend fun populateDatabase(taskDao: TaskDao) {
-        taskDao.insertTask(Task(1, "Task 1", "Description 1", System.currentTimeMillis()))
-        taskDao.insertTask(Task(2, "Task 2", "Description 2", System.currentTimeMillis()))
-        taskDao.insertTask(Task(3, "Task 3", "Description 3", System.currentTimeMillis()))
+        for (i in 0..10000) {
+            taskDao.insertTask(Task(i, "Task $i", "Description $i", System.currentTimeMillis()))
+        }
     }
 
     @Provides
