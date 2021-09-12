38199e50264cd79b8effac70da0b71202dec6559
Trung Doan <doanpt93cntt@gmail.com>
2021-09-12 17:53:27 +0700

    implement use case in domain layer

---
+++ app/src/main/java/com/ddona/architect/domain/use_case/get_coin/GetCoinsUserCase.kt
@@ -0,0 +1,27 @@
+package com.ddona.architect.domain.use_case.get_coin
+
+import com.ddona.architect.common.Resource
+import com.ddona.architect.data.remote.dto.toCoinDetail
+import com.ddona.architect.domain.model.CoinDetail
+import com.ddona.architect.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUserCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading<CoinDetail>())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success<CoinDetail>(coin))
+        } catch (e: HttpException) {
+            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error<CoinDetail>("Couldn't reach server. Check your internet connection."))
+        }
+    }
+}
---
+++ app/src/main/java/com/ddona/architect/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,27 @@
+package com.ddona.architect.domain.use_case.get_coins
+
+import com.ddona.architect.common.Resource
+import com.ddona.architect.data.remote.dto.toCoin
+import com.ddona.architect.domain.model.Coin
+import com.ddona.architect.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading<List<Coin>>())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success<List<Coin>>(coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error<List<Coin>>("Couldn't reach server. Check your internet connection."))
+        }
+    }
+}
