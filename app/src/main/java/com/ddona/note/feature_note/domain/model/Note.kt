5e3d46a062a54945de9f861af016595961821055
Trung Doan <doanpt93cntt@gmail.com>
2021-09-26 09:46:58 +0700

    Domain: Add note entities

---
+++ app/src/main/java/com/ddona/note/feature_note/domain/model/Note.kt
@@ -0,0 +1,18 @@
+package com.ddona.note.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ddona.note.ui.theme.*
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
