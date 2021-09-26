d846a1e37286282c738d9f02459b9bb1d59855d5
Trung Doan <doanpt93cntt@gmail.com>
2021-09-26 09:38:19 +0700

    init jetpack compose project

---
+++ app/src/main/java/com/ddona/note/ui/theme/Theme.kt
@@ -0,0 +1,26 @@
+package com.ddona.note.ui.theme
+
+import androidx.compose.material.darkColors
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
+)
+
+@Composable
+fun NoteCleanArchitectTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
