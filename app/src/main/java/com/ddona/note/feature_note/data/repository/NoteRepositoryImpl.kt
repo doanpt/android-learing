01c1d9098a8b956e1aff4c87a6a5374fb55d41d2
Trung Doan <doanpt93cntt@gmail.com>
2021-09-26 10:00:30 +0700

    Data: Implement Note Repository Impl

---
+++ app/src/main/java/com/ddona/note/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.ddona.note.feature_note.data.repository
+
+import com.ddona.note.feature_note.data.db.NoteDao
+import com.ddona.note.feature_note.domain.model.Note
+import com.ddona.note.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(
+    private val dao: NoteDao
+) : NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return dao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        dao.insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        dao.deleteNote(note)
+    }
+}
