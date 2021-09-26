ad00ca652bd9eed7b0a5e8099c3e42ca641d040a
Trung Doan <doanpt93cntt@gmail.com>
2021-09-26 10:02:12 +0700

    Implement Hilt to use Room DB

---
+++ app/src/main/java/com/ddona/note/NoteApplication.kt
@@ -0,0 +1,8 @@
+package com.ddona.note
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApplication: Application() {
+}
---
+++ app/src/main/java/com/ddona/note/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.ddona.note.di
+
+import android.app.Application
+import androidx.room.Room
+import com.ddona.note.feature_note.data.db.NoteDatabase
+import com.ddona.note.feature_note.data.repository.NoteRepositoryImpl
+import com.ddona.note.feature_note.domain.repository.NoteRepository
+import com.ddona.note.feature_note.domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository),
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
+        )
+    }
+}
